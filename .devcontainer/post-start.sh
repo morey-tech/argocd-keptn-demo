710ca050d19999da3c88330966b7451a90908f05
Nicholas Morey <nicholas@morey.tech>
2023-11-22 20:51:21 +0000

    fix: try older chart version that works for kargo
    Signed-off-by: Nicholas Morey <nicholas@morey.tech>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,10 +7,11 @@ echo "post-start start" >> ~/status
 kind delete cluster --name argocd-keptn
 kind create cluster --config kind-cluster.yaml
 kubectl apply -k argocd
-kubectl apply -f apps.yaml
 
 kubectl wait deployment -n argocd --all --for=condition=Available=True --timeout=90s
 ARGOCD_ADMIN_PASSWORD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 echo "${ARGOCD_ADMIN_PASSWORD}" > ~/argo-cd-admin-password.txt
+
+kubectl apply -f apps.yaml
 
 echo "post-start complete" >> ~/status
